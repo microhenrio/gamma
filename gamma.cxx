54a8aff81094324da7c28cc6a07a63b801665c17
Abel Rodriguez <microhenrio@protonmail.com>
2022-06-06 21:39:38 +0200

    added glade

--- gamma.cxx
+++ gamma.cxx
@@ -1,41 +1,49 @@
 /*
  * gamma.cxx
  * 
- * Copyright 2022  <pi@raspberrypi>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
+ * Copyright 2022  <hexentronics.com>
+ * 	
+ * User interface to communicate and configure the electronic
+ * boardgame Helvetios (https://www.hexentronics.com/en/helvetios/)
  * 
  */
-
-
+ 
 #include <iostream>
+#include <string>
+#include <filesystem>
+#include <unistd.h>
 #include <gtk/gtk.h>
+//#include <usb-1.0/usb.h>
+#include <libusb-1.0/libusb.h>
 
 using namespace std;
+
+/**
+ * @brief To manage the exit of the program
+ */
+void exit_handler (int ev, void *arg)
+{
+	printf("\nApadeu!\n");
+}
 
 int main(int argc, char **argv)
 {
 	GtkBuilder *builder;
 	GtkWidget  *window;
-	std::cout << "Hello World2!" <<  std::endl;
+	//char *cwd = get_current_dir_name();
+	//char path[256];
 	
+	//strcpy(path, cwd);
+	//strcat(path,"/gamma_hmi.glade");
+	//cout << "Current working directory: " << path << endl;
+    //free(cwd);
+    libusb_init(NULL);
+    
+	on_exit(exit_handler, NULL);										// on exit, a handler to make a safe stop
+
 	gtk_init (&argc, &argv);											// pass arguments to gtk
 	
-	builder = gtk_builder_new_from_file ("gamma_hmi.glade");		// loads xml with hmi info
+	builder = gtk_builder_new_from_file ( "gamma_hmi.glade");//path );		// loads xml with hmi info
 	/* Connect signal handlers to the constructed widgets. */
 	window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
 	//gtk_builder_connect_signals(builder, NULL); 						//To connect the signals defined in the .glade, but signals callbacks are already defined in code
